801d4309c974fd24ae23d592dcc17586b407af80
michael-mcmasters <jmcmasters411@gmail.com>
2020-10-29 15:40:27 -0400

    Finish getTriangle() and have it pass all tests (see the comment)

--- src/main/java/com/returnready/loopthereitis/TriangleUtilities.java
+++ src/main/java/com/returnready/loopthereitis/TriangleUtilities.java
@@ -2,9 +2,11 @@ package com.returnready.loopthereitis;
 
 public class TriangleUtilities {
 
+    // numberOfRows for this method seems like it should be numberOfColumns minus 1.
+    // By doing this I pass all tests. The commented out code below it attempts to the param as rows, but that only passes 1 of 2 tests.
     public static String getTriangle(int numberOfRows) {
         String stars = "";
-        int numberOfColumns = 9;
+        int numberOfColumns = numberOfRows - 1;
         for (int i = 1; i <= numberOfColumns; i++) {
             for (int j = 1; j <= i; j++) {
                 stars += "*";
@@ -13,6 +15,22 @@ public class TriangleUtilities {
         }
         System.out.println(stars);
         return stars;
+        
+        // ** This only passes 1 test **
+        // String stars = "";
+        // int numberOfStars = 1;
+        // for (int i = 0; i < numberOfRows; i++) {
+        //     for (int j = 0; j < numberOfStars; j++) {
+        //         stars += "*";
+        //     }
+        //     numberOfStars++;
+        //     stars += "\n";
+        //     if (numberOfStars > 9) {
+        //         break;
+        //     }
+        // }
+        // System.out.println(stars);
+        // return stars;
     }
 
     public static String getRow(int numberOfStars) {
