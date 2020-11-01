26072e359ad5bed4627e6600fe2ba01c04bb84b3
michael-mcmasters <jmcmasters411@gmail.com>
2020-10-31 20:22:58 -0400

    Refactor so that it loops through rows first and columns second. Since that is more intuitive.

--- src/main/java/com/returnready/loopthereitis/TableUtilities.java
+++ src/main/java/com/returnready/loopthereitis/TableUtilities.java
@@ -2,26 +2,26 @@ package com.returnready.loopthereitis;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        int columnSize = 5;
         int rowSize = 5;
-        return getMultiplicationTableInRange(columnSize, rowSize);
+        int columnSize = 5;
+        return getMultiplicationTableInRange(rowSize, columnSize);
     }
 
     public static String getLargeMultiplicationTable() {
-        int columnSize = 10;
         int rowSize = 10;
-        return getMultiplicationTableInRange(columnSize, rowSize);
+        int columnSize = 10;
+        return getMultiplicationTableInRange(rowSize, columnSize);
     }
 
     public static String getMultiplicationTable(int tableSize) {
         return getMultiplicationTableInRange(tableSize, tableSize);
     }
-    
-    private static String getMultiplicationTableInRange(int columnSize, int rowSize) {
+
+    private static String getMultiplicationTableInRange(int rowSize, int columnSize) {
         String table = "";
-        for (int column = 1; column <= columnSize; column++) {
-            for (int row = 1; row <= rowSize; row++) {
-                int result = column * row;
+        for (int row = 1; row <= rowSize; row++) {
+            for (int column = 1; column <= columnSize; column++) {
+                int result = row * column;
                 if (result < 100) {
                     if (result < 10) {
                         table += "  ";
